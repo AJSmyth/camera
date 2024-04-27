def0f0adbf39d7ca2b25c8364033e92166b5c91a
AJ Smyth <ajsmyth@uci.edu>
2024-04-27 05:02:28 +0100

    Negotiate format

--- main.cpp
+++ main.cpp
@@ -11,24 +11,22 @@
 #include <libv4l2.h>
 #include <errno.h>
 
+#define check_err(err) if (err < 0) printf("LINE %d: Error: %s\n", __LINE__, strerror(errno))
+#define return_on_err(err) if (err < 0) return err
+
 void print_formats(int fd);
+int set_image_fmt(int fd);
 
 int main() {
+    // open fd
 	int fd = open("/dev/video0", O_RDWR);
 	if (fd < 0) {
 		printf("FERRL: %s\n", strerror(errno));
 	}
-	v4l2_input *inp = (v4l2_input*)malloc(sizeof(v4l2_input));
-	inp->index = 0;
-	int err = ioctl(fd, VIDIOC_ENUMINPUT, inp); 
-	if (err == 0) {
-		printf("Type %d\n", inp->type);
-	}
-	else {
-		printf("ERR: %s\n", strerror(errno));
-	}
 
 	print_formats(fd);
+
+    set_image_fmt(fd);
 
 	close(fd);
 	return 0;
@@ -62,4 +60,33 @@ void print_formats(int fd) {
         (fmt->pixelformat & 0x00'00'FF'00) >> 8,
         (fmt->pixelformat & 0x00'00'00'FF));	
     }
+    free(fmt);
 }
+
+int set_image_fmt(int fd) {
+    // using single planar YUYV 4:2:2
+    v4l2_format *fmt = (v4l2_format*)malloc(sizeof(v4l2_format));
+    fmt->type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    int err = ioctl(fd, VIDIOC_G_FMT, fmt);
+    check_err(err);
+    return_on_err(err);
+
+    // change to desired format
+    if (fmt->type == V4L2_BUF_TYPE_VIDEO_CAPTURE) {
+        fmt->fmt.pix.width = 640;
+        fmt->fmt.pix.height = 480;
+        fmt->fmt.pix.pixelformat = v4l2_fourcc('V','Y','U','Y');
+        fmt->fmt.pix.field = V4L2_FIELD_NONE; //progressive (non-interlaced)
+
+        // attempt to write format
+        int err = ioctl(fd, VIDIOC_S_FMT, fmt);
+        check_err(err);
+        return_on_err(err);
+    }
+    else {
+        printf("Not VID_CAPTURE");
+    }
+    return 0;
+}
+
+
