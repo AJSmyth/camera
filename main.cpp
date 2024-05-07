23aa46f1df36e0bf978aece978e87ab64cf3136c
AJ Smyth <ajsmyth@uci.edu>
2024-05-07 18:02:45 +0100

    Add some muxer functions

--- main.cpp
+++ main.cpp
@@ -14,9 +14,20 @@
 #include <cassert>
 
 #include "gpio.h"
+#define DEBUG
 #define check_err(err) if (err < 0) printf("LINE %d: Error: %s\n", __LINE__, strerror(errno))
-#define return_on_err(err) if (err < 0) exit(EXIT_FAILURE)
+#define return_on_err(err) if (err < 0) exit(EXIT_FAILURE)  //TODO: make this less intense
 #define read_camera_reg(addr) read_camera_reg_fd(i2c_fd, addr)
+#ifdef DEBUG
+#define debug_print(f,...) printf(f, __VA_ARGS__)
+#else
+#define debug_print(...)
+#endif
+
+#define PIN_SEL 4 //BCM Pin 7
+#define PIN_OE 17 //BCM Pin 11
+#define ADDR_CAM 0x36
+#define ADDR_MUX 0x70
 
 
 const int MIN_BUF_COUNT = 3;
@@ -24,6 +35,7 @@ const int MIN_BUF_COUNT = 3;
 void print_formats(int fd);
 int set_image_fmt(int fd);
 uint8_t read_camera_reg_fd(int fd, uint16_t addr);
+int gpio_setup(); 
 
 typedef struct {
     void *start;
@@ -34,9 +46,6 @@ int main() {
     // open i2c fd
     int i2c_fd = open("/dev/i2c-1", O_RDWR);
 
-    gpioInitialise();
-    printf("REEE: %x\n", gpioGetMode(17));
-    printf("REEE: %x\n", gpioGetMode(10));
     // open camera fd
 	int fd = open("/dev/video0", O_RDWR);
 	if (fd < 0) {
@@ -216,7 +225,7 @@ int set_image_fmt(int fd) {
 }
 
 uint8_t read_camera_reg_fd(int fd, uint16_t addr) {
-    int err = ioctl(fd, I2C_SLAVE, 0x36);
+    int err = ioctl(fd, I2C_SLAVE, ADDR_CAM);
     check_err(err);
     return_on_err(err);
     uint8_t buf[2] = {addr >> 8, addr & 0xff};
@@ -237,4 +246,51 @@ uint8_t read_camera_reg_fd(int fd, uint16_t addr) {
     check_err(err);
     return_on_err(err);
     return buf[0];
+}
+
+int gpio_setup(void) {
+    int err = gpioInitialise();
+    if (0 != err) {
+        return -1;
+    }
+    gpioSetMode(PIN_SEL, PI_OUTPUT);
+    gpioSetMode(PIN_OE, PI_OUTPUT);
+    sleep(1);
+    if (gpioGetMode(PIN_SEL) != PI_OUTPUT && gpioGetMode(PIN_OE) != PI_OUTPUT) {
+        perror("Failed to set GPIO to output");
+        return -1;
+    }
+}
+
+//UC-444 Specific methods
+void enable_cam_a(void) {
+    gpioWrite(PIN_SEL, 0);
+    gpioWrite(PIN_OE, 0);
+}
+
+int i2c_sel_cam_a(int fd) {
+    int err = ioctl(fd, I2C_SLAVE, ADDR_MUX);
+    check_err(err);
+    if (err < 0) return -1; 
+    
+    uint8_t data = 0x01;
+    return write(fd, &data, 1);
+}
+
+void enable_cam_b(void) {
+    gpioWrite(PIN_SEL, 1);
+    gpioWrite(PIN_OE, 0);
+}
+
+int i2c_sel_cam_b(int fd) {
+    int err = ioctl(fd, I2C_SLAVE, ADDR_MUX);
+    check_err(err);
+    if (err < 0) return -1; 
+    
+    uint8_t data = 0x02;
+    return write(fd, &data, 1);
+}
+
+int disable_cams(void) {
+    gpioWrite(PIN_OE, 1);
 }
